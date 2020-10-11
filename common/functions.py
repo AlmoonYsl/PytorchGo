1aa6149fef010a39bc752a2fed2e9a831ba4ad06
Administrator <784748497@qq.com>
2020-10-11 21:45:21 +0800

    init

---
+++ common/functions.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+# Sigmoid y = 1 / e^(-x)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
+
+
+# Relu  y = min(0, x)
+def relu(x):
+    return np.maximum(0, x)
+
+
+def relu_grad(x):
+    grad = np.zeros(x)
+    grad[x >= 0] = 1
+    return grad
+
+
+# Soft Max
+def soft_max(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+    x = x - np.max(x)
+    return np.exp(x) / np.sum(np.exp(x))
+
+
+# 1/2 * sum[(y - t) ^ 2]
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y - t) ** 2)
+
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    # if t is one-hot-vector, get the index of the max
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+    # the amount of data
+    batch_size = y.shape[0]
+    print(y[np.arange(batch_size), t])
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+
+def soft_max_loss(x, t):
+    y = soft_max(x)
+    return cross_entropy_error(y, t)
+
+
+
---
+++ test/test.py
@@ -0,0 +1,7 @@
+from common.functions import *
+import numpy as np
+
+if __name__ == '__main__':
+    x = np.array([1, 0, 0, 0, 0])
+    t = np.array([0, 0, 0, 0, 0])
+    print(cross_entropy_error(x, t))
